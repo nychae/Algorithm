10eebb72ffc949246aa236b766742af4f48fddb9
nychae <51692255+nychae@users.noreply.github.com>
2024-02-28 16:41:20 +0900

    [level 0] Title: 개미 군단, Time: 0.02 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int hp) {
+        int answer = 0;
+        int[] ants = {5, 3, 1};
+        
+        for(int ant: ants) {
+            if(hp >= ant) {
+                answer += hp/ant;
+                hp %= ant;    
+            }
+        }
+       
+        return answer;
+    }
+}
