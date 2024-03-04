bedfa790fe176238ca03b3bbb8d56bae4d0f2679
nychae <51692255+nychae@users.noreply.github.com>
2024-03-04 11:01:55 +0900

    [level 0] Title: 공 던지기, Time: 0.02 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/0/120843. 공 던지기/공 던지기.java
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.java
@@ -1,7 +1,7 @@
 class Solution {
     public int solution(int[] numbers, int k) {
         int answer = 0;
-        int index = k*2 - 2 >= numbers.length? (k*2 - 2) % numbers.length: k*2 -2;
+        int index = (k*2 - 2) % numbers.length;
         
         return numbers[index];
     }
