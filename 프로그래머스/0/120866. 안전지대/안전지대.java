828d85508eee8a11b3166c9d3c3dc2b557690248
nychae <51692255+nychae@users.noreply.github.com>
2024-03-07 11:52:39 +0900

    [level 0] Title: 안전지대, Time: 2.27 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/0/120866. 안전지대/안전지대.java
+++ 프로그래머스/0/120866. 안전지대/안전지대.java
@@ -1,6 +1,28 @@
+import java.util.*;
 class Solution {
     public int solution(int[][] board) {
-        int answer = 0;
-        return answer;
+    int answer = 0;
+        List<int[]> mineList = new ArrayList<>();
+        for(int i=0;i<board.length;i++) {
+            for(int j=0;j<board[i].length;j++) {
+                if(board[i][j] == 1) {
+                    mineList.add(new int[]{i, j});
+                }
+            }
+        }
+
+        mineList.stream().forEach(mine -> {
+            for(int i=mine[0]-1;i<=mine[0]+1;i++) {
+                if(i == -1 ) continue;
+                for(int j=mine[1]-1;j<=mine[1]+1;j++) {
+                    if(j == -1) continue;
+                    if(i < board.length && j < board[0].length) {
+                        board[i][j] = 1;
+                    }
+                }
+            }
+        });
+        
+        return (int) Arrays.stream(board).flatMapToInt(s -> Arrays.stream(s)).filter(value -> value == 0).count();
     }
 }
