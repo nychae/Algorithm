58f7488f0506627499a516fc1dcc0ce776a7c991
nychae <51692255+nychae@users.noreply.github.com>
2024-03-12 11:41:11 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.03 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.java
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.java
@@ -2,8 +2,7 @@ import java.util.*;
 class Solution {
     public int solution(int a, int b) {
         int answer = 0;
-        List<Integer> answerList = new ArrayList();
-        
+
         int min = Math.min(a, b);
         int max = 1;
         for(int i=2; i<=min; i++) {
@@ -11,22 +10,21 @@ class Solution {
                 max = max < i? i: max;
             }
         }
-        
-        b /= max;
-        int i = 2;
-        while(b > 1) {
-            if(b%i == 0) {
-                for(int j=2;j<i;j++) {
-                    if(i%j == 0) break;
-                }
 
-                b/=i;
-                answerList.add(i);
-            } else {
-                i++;
+        b /= max;
+
+        while(b > 1) {
+            if(b % 5 == 0) {
+                b /= 5;
+                continue;
             }
+
+            if(b % 2 == 0) {
+                b /= 2;
+                continue;
+            }
+            return 2;
         }
-        
-        return answerList.stream().allMatch(n -> n ==2 || n ==5)? 1: 2;
+       return 1;
     }
 }
