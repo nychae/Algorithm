2ded59c395b75b33a30864039686bbd26e530dfa
nychae <51692255+nychae@users.noreply.github.com>
2024-03-12 10:52:44 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 3.17 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
+++ 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
@@ -3,8 +3,9 @@ class Solution {
     public int solution(int[][] lines) {
         List<int[]> list = new ArrayList();
         List<int[]> dupl = new ArrayList();
-        for(int i=0; i<lines.length; i++) {
-            for(int j=lines[i][0];j<lines[i][1];j++) {
+        
+        for(int[] line: lines) {
+            for(int j=line[0];j<line[1];j++) {
                 int[] value = {j, j+1};
                 if(list.stream().anyMatch(s -> Arrays.equals(s, value))) {
                     if(dupl.stream().noneMatch(s -> Arrays.equals(s, value))) {
