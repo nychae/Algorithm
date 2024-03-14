4aaee4e34c4b89bdb6d30582a868bf5e5d274a61
nychae <51692255+nychae@users.noreply.github.com>
2024-03-14 16:14:06 +0900

    [level 0] Title: 약수 구하기, Time: 2.58 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -5,10 +5,7 @@ class Solution {
         Set<Integer> answerList = new HashSet<>();
         
         for(int i=1;i<=n;i++) {
-            if(n % i == 0) {
-                answerList.add(i);
-                answerList.add(n/i);
-            }
+            if(n % i == 0) answerList.add(i);
         }
         return answerList.stream().sorted().mapToInt(Integer::intValue).toArray();
     }
