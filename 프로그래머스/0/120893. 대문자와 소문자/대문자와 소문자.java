598e5ca0cd070476abdb1ea93000f025ac6647ed
nychae <51692255+nychae@users.noreply.github.com>
2024-03-14 10:27:28 +0900

    [level 0] Title: 대문자와 소문자, Time: 17.93 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        for(char c: my_string.toCharArray()) {
+            answer += Character.isUpperCase(c)? Character.toLowerCase(c): Character.toUpperCase(c);
+
+        }
+        return answer;
+    }
+}
