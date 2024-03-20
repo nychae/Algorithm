d0e1e42768c66ceca94d2de26954c7fc95cea381
nychae <51692255+nychae@users.noreply.github.com>
2024-03-20 15:18:04 +0900

    [level 0] Title: 커피 심부름, Time: 0.04 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(String[] order) {
+        int answer = 0;
+        
+        for(String menu: order) {
+            answer += menu.contains("cafelatte")? 5000: 4500;
+        }
+        return answer;
+    }
+}
