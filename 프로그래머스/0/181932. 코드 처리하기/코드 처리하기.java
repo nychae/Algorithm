da300145355abe744f216fa697196c39a59a8480
nychae <51692255+nychae@users.noreply.github.com>
2025-04-14 23:17:24 +0900

    [level 0] Title: 코드 처리하기, Time: 423.14 ms, Memory: 382 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String solution(String code) {
+        String answer = "";
+        boolean mode = false;
+        
+        for(int i=0;i<code.length();i++) {
+            if(code.charAt(i) == '1') {
+                mode = !mode;
+            } else {
+                if(mode && i % 2 == 1) {
+                    answer += code.charAt(i);
+                } 
+                
+                if(!mode && i % 2 == 0) {
+                    answer += code.charAt(i);
+                }
+            }
+        }
+        
+        return answer.length() == 0? "EMPTY": answer;
+    }
+}
